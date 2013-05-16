ffa6b43dff1e826b3e9b6ac50618d6a79c5a633f
Erik Pukinskis <erik@snowedin.net>
2013-05-16 02:15:48 -0700

    Process css and generate package.js

--- package.js
+++ package.js
@@ -4,15 +4,46 @@ Package.describe({
 
 Package.on_use(function (api) {
 
+
   api.add_files([
-    'css/bootstrap.css',
-    'css/flat-ui.css',
-    'images/checkbox-2x.png',
-    'js/jquery-1.8.2.min.js',
-    'js/jquery-ui-1.10.0.custom.min.js',
-    'js/jquery.dropkick-1.0.0.js',
-    'js/custom_checkbox_and_radio.js',
-    'js/custom_radio.js',
-    'js/jquery.placeholder.js'
+    // FILES
+    'assets/js/jquery-1.8.2.min.js',
+    'assets/js/jquery-ui-1.10.0.custom.min.js',
+    'assets/images/select/toggle.png',
+    'assets/fonts/Flat-UI-Icons-16.eot',
+    'assets/fonts/Flat-UI-Icons-16.svg',
+    'assets/fonts/Flat-UI-Icons-16.ttf',
+    'assets/fonts/Flat-UI-Icons-16.woff',
+    'assets/fonts/Flat-UI-Icons-24.dev.svg',
+    'assets/fonts/Flat-UI-Icons-24.eot',
+    'assets/fonts/Flat-UI-Icons-24.svg',
+    'assets/fonts/Flat-UI-Icons-24.ttf',
+    'assets/fonts/Flat-UI-Icons-24.woff',
+    'assets/images/checkbox-2x.png',
+    'assets/images/checkbox.png',
+    'assets/images/pager/next.png',
+    'assets/images/pager/previous.png',
+    'assets/images/radio-2x.png',
+    'assets/images/radio.png',
+    'assets/fonts/Flat-UI-Icons-16.dev.svg',
+    'assets/images/toggle/block-off.png',
+    'assets/images/toggle/block-on.png',
+    'assets/images/toggle/icon-off-2x.png',
+    'assets/images/toggle/icon-off.png',
+    'assets/images/toggle/icon-on-2x.png',
+    'assets/images/toggle/icon-on.png',
+    'assets/js/application.js',
+    'assets/js/bootstrap-tooltip.js',
+    'assets/js/custom_checkbox_and_radio.js',
+    'assets/js/custom_radio.js',
+    'assets/js/html5shiv.js',
+    'assets/js/icon-font-ie7.js',
+    'assets/css/bootstrap.css',
+    'assets/css/flat-ui.css',
+    'assets/js/jquery.dropkick-1.0.0.js',
+    'assets/js/jquery.placeholder.js',
+    'assets/js/jquery.tagsinput.js',
+    'assets/js/lte-ie7-24.js'
+    // END FILES
   ], 'client');
 });
